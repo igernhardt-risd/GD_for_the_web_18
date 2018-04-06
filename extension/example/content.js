7a289bc989a7564727b8c26b0428c22653d2550b
Isaiah <flyanim@gmail.com>
2018-04-06 11:21:42 -0400

    Raw html

--- extension/example/content.js
+++ extension/example/content.js
@@ -1,21 +1,28 @@
-// replace images when the page loads
-$('img').attr('src', 'https://i.imgur.com/8QMMdNv.jpg');
+// var markup = document.documentElement.innerHTML;
+// // replace images when the page loads
+// $('img').attr('src', 'https://i.imgur.com/8QMMdNv.jpg');
 
-// replace text in the page when it loads
-$('body').children().each(function () {
-	// replace the '@' sign with a '$' sign
-	$(this).html( $(this).html().replace(/@/g,'$') );
-});
+// // replace text in the page when it loads
+// $('body').children().each(function () {
+// 	// replace the '@' sign with a '$' sign
+// 	$(this).html( $(this).html().replace(/@/g,'$') );
+// });
 
-// when you click on an image hide it using CSS
-$('img').click(function() {
-	$(this).addClass('hide');
-});
+// // when you click on an image hide it using CSS
+// $('img').click(function() {
+// 	$(this).addClass('hide');
+// });
 
-// when you click on the page 'body' apply .gradient to it from the CSS
-$('body').click(function() {
-	$(this).addClass('gradient');
-});
+// // when you click on the page 'body' apply .gradient to it from the CSS
+// $('body').click(function() {
+// 	$(this).addClass('gradient');
+// });
+
+//$('body').html(markup);
+var e = document.body;
+    e.parentNode.removeChild(e);
+    document.body = document.createElement("body");
+    document.body.innerText = markup;
 
 
 
